93e2274dac665d81d5d09ba6c11c6152bd7b465e
Manel <manuelss.23.bil@gmail.com>
2023-09-20 20:39:59 -0300

    Implementing database connection class

---
+++ src/core/database/db_connection.php
@@ -0,0 +1,37 @@
+<?php
+
+class DbConnection
+{
+    public static $pdo;
+    public $hostname;
+    public $database;
+    public $user;
+    public $password;
+
+    public function __construct()
+    {
+        $this->hostname = 'server ip address';
+        $this->database = 'database name';
+        $this->user = 'user to be used in connection';
+        $this->password = 'password that matchs with user';
+    }
+
+    public function openConnection()
+    {
+        try {
+            if (!static::$pdo) {
+                $dsn = 'mysql:host=' . $this->hostname . ';dbname=' . $this->database;
+                $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+                static::$pdo = new PDO($dsn, $this->user, $this->password, $options);
+            }
+        } catch (PDOException $PDOerror) {
+            die("Erro ao realizar a conexão com o banco de dados (PDO exception) " . $PDOerror->getMessage());
+        } catch (Exception $error) {
+            die("Erro ao realizar a conexão com o banco de dados (General exception) " . $error->getMessage());
+        }
+    }
+
+}
+
+?>
